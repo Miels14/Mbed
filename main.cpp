6f93d64c96a596e58edb7b2bde497b01dc45d1ef
Miel <BC278810@intra.cea.fr>
2025-01-23 10:24:38 +0100

    Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+
+UnbufferedSerial Teraterm(USBTX,USBRX,9600);
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+        Teraterm.write("Hello World\n", 5);
+        ThisThread::sleep_for(1000ms);
+    }
+}
+
