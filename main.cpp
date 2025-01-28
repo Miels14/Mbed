6c10fe1edc9e14a2b5fd46122239c05e8196104c
Miel <benjamin.cao.pro@gmail.com>
2025-01-28 12:57:14 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -4,31 +4,26 @@
 #include <cstring>
 #include "LED.h"
 
-UnbufferedSerial Teraterm(USBTX, USBRX,9600);
+UnbufferedSerial Teraterm(USBTX, USBRX,115200);
 AnalogIn adc_temperature(PA_3);
 Ticker mytick;
-
+float adc_value = 0, voltage = 0, temperature = 0;
+ char temperature_value[200];
 // main() runs in its own thread in the OS
 int main() 
 {
     Teraterm.format(8,SerialBase::None,1);
-    float adc_value = 0, voltage = 0, temperature = 0;
-    char temperature_value[200];
-    Teraterm.write("Connexion a TeraTerm\r\n",21);
+    mytick.attach([]()
+    {
+        sprintf(temperature_value,"$%4.2f;", temperature);
+        Teraterm.write(temperature_value, strlen(temperature_value));
+    },20ms);
    while (true)
    {
-       
        adc_value = adc_temperature.read();
        voltage = adc_value * 3.3;
-       temperature = voltage * 100;
-       if (Teraterm.writeable()==1) 
-       {
-           sprintf(temperature_value,"La valeur de la temperature est = %.2fC et la valeur de la tension est = %.3fV et la valeur de l'adc %.3f\n"
-           , temperature,voltage,adc_value);
-           Teraterm.write(temperature_value, strlen(temperature_value));
-       }
+       temperature = voltage * 100; 
        Led_affichage(temperature);
-
-        ThisThread::sleep_for(1000ms);
+        ThisThread::sleep_for(10ms);
    }
 }
