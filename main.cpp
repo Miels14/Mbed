989fd92bec32cfa0e915aa544aaf0080bf9cdc3f
Miel <benjamin.cao.pro@gmail.com>
2025-01-31 09:07:54 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include "mbed.h"
 
-UnbufferedSerial Teraterm(USBTX,USBRX,9600);
-EventFlags Flags;
+UnbufferedSerial Teraterm(USBTX,USBRX,9600); //
 
 // main() runs in its own thread in the OS
 int main()
 {
     while (true) {
-        Teraterm.write("Hello World\n", 11);
+        Teraterm.write("Hello World\n", 12);
         ThisThread::sleep_for(1000ms);
     }
 }
